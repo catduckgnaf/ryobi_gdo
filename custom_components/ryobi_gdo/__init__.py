f32e259db809cb2ea13aa20bfaef19157062b5a6
catduckgnaf <github@catduck.org>
2024-01-07 11:18:42 -0500

    Update __init__.py

--- custom_components/ryobi_gdo/__init__.py
+++ custom_components/ryobi_gdo/__init__.py
@@ -12,7 +12,6 @@ from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 from .api import RyobiApiClient
 from .const import DOMAIN
-from .coordinator import RyobiDataUpdateCoordinator
 
 PLATFORMS: list[Platform] = [
     Platform.COVER,
