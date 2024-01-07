f4ce24b6242432fddf13d42128e35c8a85e33d77
catduckgnaf <github@catduck.org>
2024-01-07 11:25:35 -0500

    add coord

---
+++ custom_components/ryobi_gdo/coordinator.py
@@ -0,0 +1,39 @@
+"""DataUpdateCoordinator for ryobi_gdo."""
+from __future__ import annotations
+
+from datetime import timedelta
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.update_coordinator import (
+    DataUpdateCoordinator,
+    UpdateFailed,
+)
+from homeassistant.exceptions import ConfigEntryAuthFailed
+
+from .api import (
+    RyobiApiClient,
+
+)
+from .const import DOMAIN, LOGGER
+
+
+# https://developers.home-assistant.io/docs/integration_fetching_data#coordinated-single-api-poll-for-data-for-all-entities
+class RyobiDataUpdateCoordinator(DataUpdateCoordinator):
+    """Class to manage fetching data from the API."""
+
+    config_entry: ConfigEntry
+
+    def __init__(
+        self,
+        hass: HomeAssistant,
+        client: RyobiApiClient,
+    ) -> None:
+        """Initialize."""
+        self.client = client
+        super().__init__(
+            hass=hass,
+            logger=LOGGER,
+            name=DOMAIN,
+            update_interval=timedelta(minutes=5),
+        )
---
+++ custom_components/ryobi_gdo/entity.py
@@ -0,0 +1,25 @@
+"""RyobiEntity class."""
+from __future__ import annotations
+
+from homeassistant.helpers.entity import DeviceInfo
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
+
+from .const import ATTRIBUTION, DOMAIN, NAME, VERSION
+from .coordinator import RyobiDataUpdateCoordinator
+
+
+class RyobiEntity(CoordinatorEntity):
+    """RyobiEntity class."""
+
+    _attr_attribution = ATTRIBUTION
+
+    def __init__(self, coordinator: RyobiDataUpdateCoordinator) -> None:
+        """Initialize."""
+        super().__init__(coordinator)
+        self._attr_unique_id = coordinator.config_entry.entry_id
+        self._attr_device_info = DeviceInfo(
+            identifiers={(DOMAIN, self.unique_id)},
+            name=NAME,
+            model=VERSION,
+            manufacturer=NAME,
+        )
