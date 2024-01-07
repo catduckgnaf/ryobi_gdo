1c2c46d11e11850ce36236729a2ac1aac8f1fef6
catduckgnaf <github@catduck.org>
2024-01-07 12:36:22 -0500

    Update light.py

--- custom_components/ryobi_gdo/light.py
+++ custom_components/ryobi_gdo/light.py
@@ -1,5 +1,5 @@
 """
-Ryobi platform for the cover component.
+Ryobi platform for the light component.
 For more details about this platform, please refer to the documentation
 https://home-assistant.io/components/cover.ryobi_gdo/
 """
